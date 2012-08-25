2acf38b0af56c42d1c80af1102ef0332a2dcc3ad
djstrong <djstrong@gmail.com>
2012-08-25 21:10:34 +0200

    fullscreen after click and changing image with arrow keys

--- src/apod.js
+++ src/apod.js
@@ -1,3 +1,17 @@
+function setBG() {
+  var urls = JSON.parse(localStorage["urls"]);
+
+  while (urls[localStorage["index"]]==null) {
+    localStorage["index"] = (parseInt(localStorage["index"]) + 1) % urls.length;
+  }
+  
+  document.body.style.background="url('"+urls[localStorage["index"]]+"')"
+  document.body.style.backgroundPosition="center"
+  document.body.style.backgroundSize="cover"
+  document.body.style.margin = 0;
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
   var urls = JSON.parse(localStorage["urls"]);
   
@@ -6,12 +20,28 @@ document.addEventListener('DOMContentLoaded', function () {
   else {
     localStorage["index"] = (parseInt(localStorage["index"]) + 1) % urls.length;
   }
-  
-  while (urls[localStorage["index"]]==null) {
-    localStorage["index"] = (parseInt(localStorage["index"]) + 1) % urls.length;
+  setBG();
+
+  window.onclick = function()
+  {
+    if (document.webkitIsFullScreen)
+      document.webkitCancelFullScreen();
+    else 
+      document.documentElement.webkitRequestFullScreen();
   }
-  //console.log(urls[localStorage["index"]])
-  document.body.style.background="url('"+urls[localStorage["index"]]+"')"
-  document.body.style.backgroundPosition="center"
-  document.body.style.backgroundSize="cover"
+  
+  document.onkeydown = checkKey;
+
+  function checkKey(e) {
+    e = e || window.event;
+
+    if(e.keyCode=='39') {
+      localStorage["index"] = (parseInt(localStorage["index"]) + 1) % urls.length;
+      setBG();
+    }
+    else if(e.keyCode=='37') {
+      localStorage["index"] = (parseInt(localStorage["index"]) - 1 + urls.length) % urls.length;
+      setBG();
+    }
+  }
 });
